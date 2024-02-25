2e560b1f939eb3475736bd32561eebda0157c5f2
D3RPole96 <ilya.lagunov2003@gmail.com>
2024-02-26 00:32:11 +0500

    Bug fix + docker done

--- src/main/java/edu/example/hw1/api/exceptionHandlers/AppExceptionHandler.java
+++ src/main/java/edu/example/hw1/api/exceptionHandlers/AppExceptionHandler.java
@@ -1,21 +1,47 @@
 package edu.example.hw1.api.exceptionHandlers;
 
 import edu.example.hw1.api.exceptions.EntityNotFoundException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @RestControllerAdvice
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("errors",
+                ex.getBindingResult().getAllErrors().stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .filter(Objects::nonNull)
+                        .filter(s -> !s.isBlank())
+                        .toList());
+        return new ResponseEntity<>(body, status);
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException entityNotFoundException) {
         return handleException(entityNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException dataIntegrityViolationException) {
+        return handleException(dataIntegrityViolationException.getCause().getLocalizedMessage(), HttpStatus.CONFLICT);
     }
 
     private ResponseEntity<Object> handleException(String exceptionMessage, HttpStatusCode status) {
